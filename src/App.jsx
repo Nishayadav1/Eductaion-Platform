cb7e6b28ac602639512ac00f9f04df0a647b2c63
komalshukla2005 <komalshukla7518046264@gmail.com>
2025-03-24 15:00:19 +0530

    Added React setup with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold text-center text-blue-500">
+      Hello, Tailwind CSS and Vite React App!
+    </div>
+  );
+};
+
+export default App;
